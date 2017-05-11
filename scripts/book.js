ed18f33cfd8cc5982013c8cf8928d03141de1c1f
pnkjjsr <pnkj_jsr@yahoo.co.in>
2017-05-11 17:58:57 +0530

    try to improvize more.

--- scripts/book.js
+++ scripts/book.js
@@ -1,24 +1,77 @@
-var getJSON = function(url) {
-    return new Promise(function(resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('get', url, true);
-        xhr.responseType = 'json';
-        xhr.onload = function() {
-            var status = xhr.status;
-            if (status == 200) {
-                resolve(xhr.response);
-            } else {
-                reject(status);
-            }
-        };
-        xhr.send();
-    });
-};
+var getJson = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        var status = xhr.status;
+        if (status == 200) {
+            callback(xhr.response);
+        } else {
+            callback(status);
+        }
+    };
+    xhr.send();
+}
 
-getJSON('api/book.json').then(function(data) {
-    console.log(data[0]); //you can comment this, i used it to debug
+getJson('api/book.json', function(data) {
+    var book = new listing(data);
+    book.insert('date');
+    book.insert('title');
+    book.insert('author');
+    book.insert('content');
+    book.insert('tags');
+    book.insert('comment');
 
-    // result.innerText = data; //display the result in an HTML element
-}, function(status) { //error detection....
-    alert('Something went wrong.');
 });
+
+var listing = function(data) {
+    this.insert = function(el) {
+        var val = data[0][el];
+        document.getElementById(el).innerHTML = val;
+    };
+}
+
+
+
+
+// var Json = function() {
+//         // Getting URL
+//         this.getApi = function(url) {
+//             return new Promise(function(resolve, reject) {
+//                 var xhr = new XMLHttpRequest();
+//                 xhr.open('get', url, true);
+//                 xhr.responseType = 'json';
+//                 xhr.onload = function() {
+//                     var status = xhr.status;
+//                     if (status == 200) {
+//                         resolve(xhr.response);
+//                     } else {
+//                         reject(status);
+//                     }
+//                 };
+//                 xhr.send();
+//             });
+//         };
+//     }
+//     // var Insert = function(el) {
+//     //     Json.call(this);
+//     //     var val = data[0][el];
+//     //     document.getElementById(el).innerHTML = val;
+//     // };
+//     // Insert.prototype = Object.create(Json.prototype);
+//     // Insert.prototype.constructor = Insert;
+
+// // var book = new Insert('title');
+
+
+// var book = new Json();
+// book.getApi('api/book.json').then(function(data) {
+//     function insert(el) {
+//         var val = data[0][el];
+//         document.getElementById(el).innerHTML = val;
+//     };
+//     insert('title');
+//     insert('date');
+// }, function(status) {
+//     console.log('Something went wrong.');
+// });
